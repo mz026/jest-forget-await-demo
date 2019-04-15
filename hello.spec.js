36b87adb17ee8c28ea4aeae5cf15c7b210ec46bf
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2019-04-15 14:49:23 +0800

    add case for forgetting await with another spec
    result:
    
    - Jest fails the wrong spec (the naive one)
    - still gets the 'Cannot log after tests are done.' error afterward
    
    logs:
    
    > jest-forget-await-demo@1.0.0 test /Users/mz026/codes/test_codes/jest
    > jest hello.spec.js
    
      console.log setup.js:3
        browser before { key: 0.7696457364994498 }
    
      console.log hello.spec.js:3
        key when doAsync:  0.7696457364994498
    
      console.log hello.spec.js:25
        finish spec
    
      console.log setup.js:3
        browser before { key: 0.12607086539311751 }
    
      console.log hello.spec.js:3
        key when doAsync:  0.12607086539311751
    
     FAIL  ./hello.spec.js
      Greeting
        ✓ forgets await (12ms)
        ✕ a naive one (1009ms)
    
      ● Greeting › a naive one
    
        browser closed
    
          11 |       if (global.browser.key !== currentBrowserKey) {
          12 |         console.log('browser not matched')
        > 13 |         throw new Error('browser closed')
             |               ^
          14 |       } else {
          15 |         resolve(ret)
          16 |       }
    
          at Timeout._onTimeout (hello.spec.js:13:15)
    
    Test Suites: 1 failed, 1 total
    Tests:       1 failed, 1 passed, 2 total
    Snapshots:   0 total
    Time:        1.899s
    Ran all test suites matching /hello.spec.js/i.
      console.log hello.spec.js:7
        setTimeout, expected: 0.7696457364994498, current global: 0.12607086539311751
    
      console.log hello.spec.js:9
        timeout meet { key: 0.12607086539311751 }
    
      console.log hello.spec.js:12
        browser not matched
    
      ●  Cannot log after tests are done. Did you forget to wait for something async in your test?
        Attempted to log "setTimeout, expected: 0.12607086539311751, current global: 0.12607086539311751".
    
           5 |   return new Promise((resolve, reject) => {
           6 |     setTimeout(function() {
        >  7 |       console.log(`setTimeout, expected: ${currentBrowserKey}, current global: ${global.browser.key}`)
             |               ^
           8 |       if (log) {
           9 |         console.log('timeout meet', global.browser)
          10 |       }
    
          at CustomConsole.log (node_modules/@jest/console/build/CustomConsole.js:183:10)
          at Timeout.log [as _onTimeout] (hello.spec.js:7:15)

--- hello.spec.js
+++ hello.spec.js
@@ -5,7 +5,15 @@ function doAsync (ret, timeout = 500, log = true) {
   return new Promise((resolve, reject) => {
     setTimeout(function() {
       console.log(`setTimeout, expected: ${currentBrowserKey}, current global: ${global.browser.key}`)
-      resolve(ret)
+      if (log) {
+        console.log('timeout meet', global.browser)
+      }
+      if (global.browser.key !== currentBrowserKey) {
+        console.log('browser not matched')
+        throw new Error('browser closed')
+      } else {
+        resolve(ret)
+      }
     }, timeout)
   })
 }
@@ -15,5 +23,8 @@ describe('Greeting', () => {
     doAsync(1, 1000)
     expect(true).toEqual(true)
     console.log('finish spec')
+  })
+  it('a naive one', async () => {
+    await doAsync(1, 1500, false)
   })
 })
